154250951a457b48badfdf847442c8e21dd30a36
javooespir <105745198+javooespir@users.noreply.github.com>
2022-08-24 19:35:22 -0300

    Ejercicio de Practica resuelto

--- ejercicios_practica.py
+++ ejercicios_practica.py
@@ -53,8 +53,7 @@ def create_schema():
     # Cerrar la conexión con la base de datos
     conn.close()
 
-
-def fill():
+def fill(name, age, grade='', tutor=''):
     print('Completemos esta tablita!')
     # Llenar la tabla de la secundaria con al menos 5 estudiantes
     # Cada estudiante tiene los posibles campos:
@@ -69,27 +68,85 @@ def fill():
     # en el schema creado, puede obivar en algunos casos completar esos campos
 
 
+    conn = sqlite3.connect('secundaria.db')
+    c = conn.cursor()
+
+    values = [name, age, grade, tutor]
+
+    c.execute("""
+        INSERT INTO estudiante (name, age, grade, tutor)
+        VALUES (?,?,?,?);""", values)
+
+    conn.commit()
+
+    conn.close()
+
 def fetch():
     print('Comprobemos su contenido, ¿qué hay en la tabla?')
+
+
     # Utilizar la sentencia SELECT para imprimir en pantalla
     # todas las filas con todas sus columnas
     # Utilizar fetchone para imprimir de una fila a la vez
+    conn = sqlite3.connect('secundaria.db')
+    c = conn.cursor()
+
+    print('Selección de todas las columnas y filas: ')
+    c.execute('SELECT * FROM estudiante')
+    data = c.fetchall()
+    print(data)
+
+    # SELECT and read row to row
+    print('Selección y lectura de una fila a la vez mediante bucle: ')
+    # Forma 1
+    c.execute('SELECT * FROM estudiante')
+    while True:
+        row = c.fetchone()
+        if row is None:
+            break
+        print(row)
+
+    print('Captar datos desde la query: ')
+    # Forma 2
+    for row in c.execute('SELECT * FROM estudiante'):
+        print(row)
+
+    conn.close()
 
 
 def search_by_grade(grade):
     print('Operación búsqueda!')
     # Utilizar la sentencia SELECT para imprimir en pantalla
     # aquellos estudiantes que se encuentra en en año "grade"
-
     # De la lista de esos estudiantes el SELECT solo debe traer
     # las siguientes columnas por fila encontrada:
     # id / name / age
+    conn = sqlite3.connect('secundaria.db')
+    c = conn.cursor()
+
+    print('Selección de todos los "id, name, age" con filtro "grade": ')
+    c.execute('SELECT id, name, age FROM estudiante WHERE grade=?', (grade,))
+    data = c.fetchall()
+
+    print(data)
+
+    conn.close()
 
 
-def insert(grade):
+def insert(new_student):
     print('Nuevos ingresos!')
     # Utilizar la sentencia INSERT para ingresar nuevos estudiantes
     # a la secundaria
+    conn = sqlite3.connect('secundaria.db')
+    c = conn.cursor()
+
+    c.execute("""
+        INSERT INTO estudiante (name, age)
+        VALUES (?,?);""", new_student)
+
+    conn.commit()
+    # Cerrar la conexión con la base de datos
+    conn.close()
 
 
 def modify(id, name):
@@ -97,20 +154,41 @@ def modify(id, name):
     # Utilizar la sentencia UPDATE para modificar aquella fila (estudiante)
     # cuyo id sea el "id" pasado como parámetro,
     # modificar su nombre por "name" pasado como parámetro
+    conn = sqlite3.connect('secundaria.db')
+    c = conn.cursor()
 
+    rowcount = c.execute(
+        "UPDATE estudiante SET name=? WHERE id=?", (name, id)).rowcount
+
+    print("Actualización de datos", rowcount)
+
+    conn.commit()
+
+    conn.close()
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     create_schema()   # create and reset database (DB)
     # fill()
-    # fetch()
+    fill('Alberto', 16, 4, 'Salina')
+    fill('Anabelle', 18, 6, 'Gonzalez')
+    fill('Chiara', 15, 3, 'Monte')
+    fill('Omar', 17, 5, 'Sola')
+    fill('Micaela', 13, 1, 'Carballo')
 
+    fetch()
     grade = 3
     # search_by_grade(grade)
+    search_by_grade(grade)
 
     new_student = ['You', 16]
     # insert(new_student)
+    insert(new_student)
+    fetch()   # Visualizar el estudiante incroporado
 
     name = '¿Inove?'
     id = 2
     # modify(id, name)
+    modify(id, name)
+    fetch()  # Visualizr los cambios formulados en modify
+
